3e0ff3c90b1595723b63a0d6331ee0c5f5a0e8cb
Benjamin Menkuec <benjamin@menkuec.de>
2024-02-19 21:32:30 +0100

    add missing modules so that verilog-ethernet can be simulated

--- BUFGMUX.v
+++ BUFGMUX.v
@@ -27,9 +27,9 @@ module BUFGMUX (O, I0, I1, S);
     output O;
     input  I0, I1, S;
 
-    reg q0, q1;
  /* verilator lint_off UNOPTFLAT */
-   reg q0_enable, q1_enable;
+    reg q0, q1;
+    reg q0_enable, q1_enable;
  /* verilator lint_on UNOPTFLAT */
     wire q0_t, q1_t;
     reg clk_sel_in;
@@ -49,32 +49,32 @@ module BUFGMUX (O, I0, I1, S);
        assign q0_t = (clk_sel_in) ? ~S : q0;
        assign q1_t = (clk_sel_in) ? S : q1;
 
-	always @(GSR or I0 or S or q0_enable)
+	always_latch @(GSR or I0 or S or q0_enable)
  	    if (GSR)
-		q0 = 1;
+    		q0 = 1;
  	    else if (!I0)
-		q0 = !S && q0_enable;
+	    	q0 = !S && q0_enable;
 
-	always @(GSR or I1 or S or q1_enable)
+	always_latch @(GSR or I1 or S or q1_enable)
  	    if (GSR)
-		q1 = 0;
+		    q1 = 0;
  	    else if (!I1)
-		q1 = S && q1_enable;
+		    q1 = S && q1_enable;
 
-	always @(GSR or q1 or I0)
+	always_latch @(GSR or q1 or I0)
  	    if (GSR)
-		q0_enable = 1;
+		    q0_enable = 1;
 	    else if (q1)
-		q0_enable = 0;
+		    q0_enable = 0;
 	    else if (I0)
-		q0_enable = !q1;
+		    q0_enable = !q1;
 
-	always @(GSR or q0 or I1)
+	always_latch @(GSR or q0 or I1)
  	    if (GSR)
-		q1_enable = 0;
+		    q1_enable = 0;
 	    else if (q0)
-		q1_enable = 0;
+		    q1_enable = 0;
 	    else if (I1)
-		q1_enable = !q0;
+		    q1_enable = !q0;
 
 endmodule
